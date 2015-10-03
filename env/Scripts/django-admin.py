13b73353c184626c1350e8a9596341cf2cbe662e
karyotyper <simplicity_1991@yahoo.com>
2015-10-03 19:58:57 +0600

    First Commit

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!h:\shawon\django rest frameworks\django project-ii\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ fishes/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import viewsets
+from rest_framework import permissions
+from fishes.models import Fish
+from fishes.serializers import FishSerializer
+
+# Create your views here.
+class FishViewSet(viewsets.ModelViewSet):
+    # this fetches all the rows of data in the Fish table
+    queryset = Fish.objects.all()
+    serializer_class = FishSerializer
